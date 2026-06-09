67ef6f8c712e647b2444f185aa6540b2c7a8b476
Sam Westrick <shwestrick@gmail.com>
2026-06-09 11:40:21 -0400

    revert node limit

--- include/pam/utils.h
+++ include/pam/utils.h
@@ -11,8 +11,7 @@ namespace utils {
   // for granularity control
   // if the size of a node is smaller than this number then it
   // processed sequentially instead of in parallel
-  // constexpr const size_t node_limit = 100;
-  constexpr const size_t node_limit = 1000;
+  constexpr const size_t node_limit = 100;
 
   // for two input sizes of n and m, should we do a parallel fork
   // assumes work proportional to m log (n/m + 1)
